aeaa5d5bdca5d5c03dc9de8b97492a0cac70c8de
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 16:28:52 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,13 +20,14 @@ int main(void)
 	{
 		printf(" and is greater than 5");
 	}
-	else if (n % 10 == 0)
+	else if (n == 0)
 	{
-		printf("and is 0");
+		printf(" and is 0");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	}
+		printf("\n");
 	return (0);
 }
