95713ca0fc5587b53b40bd9da3c62e089eca1090
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 19:33:06 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - aprogram that prints numbers
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
