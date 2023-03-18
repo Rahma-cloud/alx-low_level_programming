4cd470ad7dbfc9f2cd87ccebbd8ca311a15c4f5e
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 17:06:07 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - an alphabet program
+ * Return: (0)
+ */
 int main(void) {
     char letter = 'a';
     int i = 0;
 
-    while (i < 26) {
-        putchar(letter);
-        letter++;
+    while (i < 26)
+    {	putchar(letter);
+	letter++;
 	i++;
     }
     
