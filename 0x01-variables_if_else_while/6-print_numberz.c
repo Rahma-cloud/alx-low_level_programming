c6cc47e4feeff93b7cf2708e43de64d670bb970d
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 19:52:45 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints out number with putchar
+ * Return (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
