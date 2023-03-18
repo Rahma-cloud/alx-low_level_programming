910cefbe21e77bb9a220afe02c434e8f504ec590
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 17:34:59 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-/** main - a program that prints both lowercase and uppercase letters
+/** 
+ * main - a program that prints both lowercase and uppercase letters
  * Return: (0)
  */
 
-int main (void)
+int main(void)
 {
 	char lowercase = 'a';
 	char uppercase = 'A';
 	int i = 0;
+
+	while (i <= 26)
+	{
+		putchar(lowercase);
+		lowercase++;
+		i++;
+	}
 	while (i <= 26)
 	{
 		putchar(uppercase);
-		putchar(lowercase);
-		lowercase++;
 		uppercase++;
 		i++;
 	}
