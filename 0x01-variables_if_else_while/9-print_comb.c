65e0f66bbd37f10b373e108bd1e25c552883f5c6
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-18 00:06:26 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,10 @@ int main(void)
         for (j = i+1; j <= 9; j++)
 	{
             putchar(i + '0');
-            putchar(',', ' ');
+            putchar(', ');
             putchar(j + '0');
             if (i != 8 || j != 9) {
-                putchar(',', ' ');
+                putchar(', ');
             }
         }
     }
