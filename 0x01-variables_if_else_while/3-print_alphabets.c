e539845496d573b46aedaf2ae0f832d0506bc2d4
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 18:08:51 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 char lowercase = 'a';
 char uppercase = 'A';
 int i = 0;
-while (i <= 52)
+while (i <= 26)
 {
 putchar(lowercase);
 lowercase++;
 i++;
 }
-while (i <= 52)
+while (i <= 26)
 {
 putchar(uppercase);
 uppercase++;
