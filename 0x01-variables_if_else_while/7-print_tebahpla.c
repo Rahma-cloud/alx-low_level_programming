3c3a8da00942e1002e41d0625715b87039e38ff4
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-03-17 20:03:33 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that reverse the output
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char letter = 'a'
+
+	for (int i = 0; i <= 'z'; i--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
