5b7e650df81f9a2917f7cbed5890d52fb2caf207
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-21 13:00:04 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter = "a";
+
+while ('a' < 'z')
+{
+	_putchar(letter);
+	letter++;
+}	
+    _putchar('\n');
+}
