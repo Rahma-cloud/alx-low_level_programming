bc1f8b4559f572a298441b61f883ed7d3680da73
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-21 16:57:13 -0700

    task6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - function
+ * @num:parameter
+ * Return: (0)
+ */
+
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	return (last_digit);
+}
