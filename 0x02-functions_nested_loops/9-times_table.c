e8be065a2824bfb82dde03b24073ee65f6c9875b
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-22 08:54:36 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,24 +7,24 @@
 void times_table(void)
 {
 	int i, result;
-	for (i = 0; i <= 10; i++)
-	{
-		result = 9 * i;
-	}
-	if (result > 9)
-	{
-		_putchar((result / 10) + '0');
-	}
-	else
-	{
-		_putchar(' ');
-        }
-	
-	_putchar((result % 10) + '0');
-	if (i != 10)
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
+
+for (i = 0; i <= 10; i++)
+{
+result = 9 * i;
+}
+if (result > 9)
+{
+_putchar((result / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((result % 10) + '0');
+if (i != 10)
+{
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
 }
