2ecea510981f8c973ae97e9cc576653e9e6a9f9b
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-21 18:04:30 -0700

    task6

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (int h = 0; h < 24; h++;)
+	for (int h = 0; h < 24; h++)
 	{
-		for (int m = 0; m < 60; m++;)
+		for (int m = 0; m < 60; m++)
 		{
 			_putchar ((h / 10) + '0');
 			_putchar ((h % 10) + '0');
