9aab767bbabdf01f3fdef7815182c4fbda033f30
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-23 12:07:27 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - a function
+ * @c:parameter
+ * Return: 0
+ */
+
+int main(void)
+{
+	if (c == 'A' && c == 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
