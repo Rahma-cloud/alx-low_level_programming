07149eed204b60c433e57acc9328a12fd5870358
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-24 04:59:37 -0700

    task12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - a function
+ * @n:parameter
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int y = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		y = n;
+		_putchar('-');
+	}
+	y /= 10;
+	if (y != 0)
+	{
+		print_number('y');
+	}
+	_putchar((unsigned int) n % 10 + '0');
+}
