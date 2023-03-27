e1ce3c846f29f8debd1629953a814f96f93c62e2
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-27 15:19:28 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - a function
+ * @str: parameter
+ * Return - 0
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int b;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = a / 2;
+	}
+	else
+	{
+		b = (a + 1) / 2;
+	}
+	while (b < a)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
