d97c5b38eb0202253fc785c36a87d10470e39a2c
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-30 12:36:37 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int x;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		i++;
 	}
-	return (0);
+	x = s1[i] - s2[i];
+	return (x);
 }
