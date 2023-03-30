445de3eb1f16664003807f9f3e06a344f20ee481
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-03-30 13:46:55 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,24 +8,26 @@
 
 char *cap_string(char *ray)
 {
-	int i = 1;
-	int x;
-	int leo[] = " \t\n,;.!?\"(){}";
+	int i = 0;
 
-	if (ray[0] >= 'a' && ray[0] <= 'z')
-	{
-		ray[0] -= 32;
-	}
 	while (ray[i] != '\0')
 	{
-		for (x = 0; leo[x] != '\0'; x++)
+		if (ray[0] >= 97 && ray[0] <= 122)
+		{		{
+			ray[0] -= 32;
+		}
+		if (ray[i] == ' ' || ray[i] == '\t' || ray[i] == '\n'
+				|| ray[i] == ',' || ray[i] == ';' || ray[i] == '.' || ray[i] == '.'
+				|| ray[i] == '!' || ray[i] == '?' || ray[i] == '"' || ray[i] == '('
+				|| ray[i] == ')' || ray[i] == '{' || ray[i] == '}')
 		{
-			if (ray[i - 1] == leo[b] && (ray[i] >= 'a' && ray[i] <= 'z'))
+			if (ray[i + 1] >= 97 && ray[i + 1] <= 122)
 			{
-				ray[a] -= 32;
+				ray[i + 1] -= 32;
 			}
 		}
 		i++;
-	}
+		}
 	return (ray);
+	}
 }
