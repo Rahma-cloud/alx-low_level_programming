47079a2b06e729fccd390cee04bad7a5d1d06f11
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-03 14:10:51 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - a function
+ * @s: param
+ * @accept: param
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int m = 0;
+
+	while (*s != '\0')
+	{
+		char *ray = accept;
+
+		while (*ray != '\0')
+		{
+			if (*s == *ray)
+			{
+				m++;
+			}
+			ray++;
+		}
+	if (*ray == '\0')
+	{
+		return (m);
+	}
+	s++;
+	}
+}
