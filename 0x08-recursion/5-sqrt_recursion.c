ec0a7b4e9e53d19c34b130554d68745d261ccba9
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-05 10:44:32 -0700

    task6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,6 +19,21 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (_sqrt_recursion(_pow_recursion(n, 1 / 2)));
+		return (_sqrt_recursion(_pow_recursion(n, 0.5)));
+	}
+}
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
