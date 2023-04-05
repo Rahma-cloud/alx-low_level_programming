a577e33dc73bcdc203324686261500fb6df9cf77
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-05 13:29:48 -0700

    task6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,38 @@
 #include "main.h"
 
 /**
+ * _st_recursion - a function
+ * @m: param
+ * @n: param
+ * Return: 0
+ */
+
+int _st_recursion(int m, int n)
+{
+	int x;
+
+	x = 0.5 * (n + m / n);
+	if (x == n)
+	{
+		return (-1);
+	}
+	else if (x == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (_st_recursion(m, x));
+	}
+}
+
+/**
  * _sqrt_recursion - a function
- * _pow_recursion - a funnction
  * @n: param
  * Return: 0
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (1);
-	}
-	else
-	{
-		return (_sqrt_recursion(_pow_recursion(n, 0.5)));
-	}
-}
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else if (y == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	return (_st_recursion(n, n / 2));
 }
