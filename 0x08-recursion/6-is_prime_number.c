2a08e6f8c870b33310a2e37d8e155b05baa40281
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-05 15:16:43 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,36 @@
 #include "main.h"
 
 /**
- * is_prime_number
+ * is_prime_number_helper - a function
+ * @a: param
+ * @b: param
+ * Return: 0
+ */
+
+int is_prime_number_helper(int a, int b)
+{
+	if (b == a)
+	{
+		return (1);
+	}
+	if (a % b == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number_helper(a, b + 1));
+}
+
+/**
+ * is_prime_number - a function
  * @n: param
  * Return: 0
  */
 
 int is_prime_number(int n)
 {
-	if (n < 2 && n % 2 == 0)
+	if (n < 2)
 	{
 		return (0);
 	}
-	else
-	{
-		return (1);
-	}
+	return (is_prime_number_helper(n, 2));
 }
