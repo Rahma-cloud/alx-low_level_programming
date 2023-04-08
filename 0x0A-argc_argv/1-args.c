90f9551c76def0e8113218bca6d3da67de25f13e
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-08 04:50:13 -0700

    task2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function
+ * @argc: param
+ * @argv: param
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
