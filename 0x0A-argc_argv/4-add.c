b088bcb8efbcec5430a30266a41c7c714670b69e
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-08 07:36:49 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - a function
+ * @argc: param
+ * @argv: param
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = sum + atoi(argv[i]);
+			if (*argv[i] < 48 || *argv[i] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
