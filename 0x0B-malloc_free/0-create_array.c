505c552707dddfce7b81a9c5aa4910fb92f313df
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-11 11:59:08 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,19 +11,18 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *p;
+	unsigned int i;
+	
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *p;
-
 	p = malloc(size * sizeof(char));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	unsigned int i;
-
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
