eb239eb6da845ed0e95814162f5f92778a9a7f7b
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-11 13:13:11 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a function
+ * @str: param
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	size_t g;
+	char *new_st;
+	size_t x;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[g] != '\0')
+	{
+		g++;
+	}
+	new_st = malloc((g + 1) * sizeof(char));
+	if (new_st == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x <= g; x++)
+	{
+		new_st[x] = str[x];
+	}
+	return (new_st);
+}
