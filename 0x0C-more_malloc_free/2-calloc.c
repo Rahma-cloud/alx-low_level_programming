c5503bf90b06e5bdf432ff029c544446c326afd4
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-13 11:02:43 -0700

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - a function
+ * @nmemb: parameter
+ * @size: param
+ * Return: 0
+ */
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *k;
+	char *p;
+	int x;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	k = malloc(nmemb * size);
+	if (k == NULL)
+	{
+		return (NULL);
+	}
+	p = (char *) k;
+	for (x = 0; x < nmemb * size; x++)
+	{
+		*(p + x) = 0;
+	}
+	return (k);
+}
