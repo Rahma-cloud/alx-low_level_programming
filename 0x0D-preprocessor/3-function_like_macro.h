f59903044bf687917b3ecf2f56e2c43c756791af
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-17 12:53:16 -0700

    task3 and 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
-#define ABS(x)  ((x) < 0 ? -(x) : (x))
-#endif
+#ifndef MAIN
+#define MAIN
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* MAIN */
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,6 @@
-#ifndef SUM
+#ifndef SUME
+#define SUME
+
 #define SUM(x, y) ((x) + (y))
-#endif
+
+#endif /* SUME */
