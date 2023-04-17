653815543df646c6d1b4361a6a4db47bf98a93cb
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-17 13:22:45 -0700

    task0

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,11 @@
+/**
+ * struct dog - a function
+ * @name: param
+ * @age: param
+ * @owner: param
+ * Return: 0
+ */
+
 struct dog
 {
 	char *name;
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DE
+#define DE
+
+/**
+ * struct dog - a function
+ * @name: param
+ * @age: param
+ * @owner: param
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DE */
