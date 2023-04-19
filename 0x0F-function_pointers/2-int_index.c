0ae99e2d80a55bb8afa3febc98dc79bcae6c4bc2
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-19 10:57:57 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,27 @@
 #include "function_pointers.h"
-#include <stddef.h>
 
 /**
  * int_index - a function
  * @array: param
  * @size: param
  * @cmp: param
+ * Return: 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
