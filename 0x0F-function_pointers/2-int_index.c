895a61b15fa2735afc7b465607b66444f8e264ce
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-19 13:14:50 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,16 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	if (array != 0 && cmp != 0)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
-		}
-		else
-		{
-			return (-1);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
-	return (i);
+	return (-1);
 }
