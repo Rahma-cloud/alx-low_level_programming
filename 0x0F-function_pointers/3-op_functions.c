ced5b83c90b7c810f5df3a9cc68c6b728311de73
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-20 00:11:04 -0700

    task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - a function
+ * @a: param
+ * @b:param
+ * Return: 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a function that substract numbers
+ * @a:param
+ * @b: param
+ * Return: 0
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function
+ * @a: param
+ * @b: param
+ * Return: 0
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function
+ * @a: param
+ * @b: param
+ * Return: 0
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a function
+ * @a: parameter
+ * @b: paramaeter
+ * Return: 0
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
