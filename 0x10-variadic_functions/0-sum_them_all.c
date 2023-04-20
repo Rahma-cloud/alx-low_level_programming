af4bc97ea0ca72c77d5f85992282871e646bbd3e
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-20 12:37:40 -0700

    task0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - a function
+ * @n: param
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int add;
+	unsigned int x;
+	va_list args;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		add = 0;
+		va_start(args, n);
+		for (x = 0; x < n; x++)
+		{
+			add = add + va_args(args, int);
+		}
+		va_end(args);
+		return (sum);
+	}
+
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+
+#endif /*FUNCTION_H */
