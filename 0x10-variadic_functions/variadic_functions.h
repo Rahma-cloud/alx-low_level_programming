1a8313d9f540771801ddb7efa3928a9209bed6ef
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-21 12:15:59 -0700

    task3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,29 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct format_specifier - format specifier structure
+ * @fmt_sign: param
+ * @fmt_func_ptr: format param
+ */
+
+typedef struct format_specifier
+{
+	char fmt_sign;
+	void (*fmt_func_ptr)(va_list);
+} fmt;
+
+/* prototypes */
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/* other prototypes */
+void char_print(va_list c);
+void int_print(va_list i);
+void float_print(va_list f);
+void string_print(va_list s);
 
 #endif /* FUNCTION_H */
