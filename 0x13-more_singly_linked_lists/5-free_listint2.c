e712cdf5585d94b82a1f093df3275baceee133e5
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-04-30 11:43:09 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function
+ * @head: parameter
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *the;
+
+	while (*head != NULL)
+	{
+		the = *head;
+		*head = (*head)->next;
+		free(the);
+	}
+	*head = NULL;
+}
