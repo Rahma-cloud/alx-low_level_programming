1ef17629997cd56c41ef6387a6e60cb908c7283a
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-05-01 05:00:24 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * pop_listint - a function
+ * @head: parameter
+ * Return: 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *tmp;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	data = (**head).n;
+	tmp = *head;
+	*head = (**head).next;
+	free(tmp);
+	return (data);
+}
