65c6f76fbe11b338a8fbb96ee99412a21aee1bbe
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-05-02 01:34:38 -0700

    task9

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - a function
+ * @head: parameter
+ * @index: parameter
+ * Return: 1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+}
