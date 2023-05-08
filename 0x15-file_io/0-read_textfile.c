ecc82d07974d4b27b591c0a97db669af43e49eaa
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-05-08 03:57:32 -0700

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function
+ * @filename: Parameter
+ * @letters: parameter
+ * Return: 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ray;
+	ssize_t fd;
+	ssize_t bw;
+	ssize_t br;
+
+	if (fd == -1)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	ray = malloc(sizeof(char) * letters);
+	br = read(fd, ray, letters);
+	bw = write(STDOUT_FILENO, ray, br);
+
+	free(ray);
+	close(fd);
+	return(bw);
+}
