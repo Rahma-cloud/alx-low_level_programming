530852f10508a212aee9e4dfec33f8764baceeac
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-05-08 04:33:16 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,18 +9,18 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bw, len = 0;
+	int fd, bw = -1;
+	ssize_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+	fd = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bw = write(fd, text_content, len);
-
 	if (fd == -1 || bw == -1)
 		return (-1);
 	close(fd);
