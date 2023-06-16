2597234af37021863f1696854172c55ae0817f80
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-15 20:09:10 -0700

    task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,18 +9,23 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
+	new->n = n;
+	new->next = NULL;
 
+	new->next = *head;
 	if (*head != NULL)
 	{
-		(*head)->prev = new_node;
+		(*head).prev = new;
 	}
 	*head = new;
+
 	return (new);
 }
