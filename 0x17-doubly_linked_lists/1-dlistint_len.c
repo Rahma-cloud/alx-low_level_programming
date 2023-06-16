3cd31da6f7a9f92641d377a0f469abb9495801c4
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-15 19:49:45 -0700

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,12 +9,17 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
-	const dlistint_t *current = h;
+	const dlistint_t *current;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+	current = h;
 	while (current != NULL)
 	{
 		count++;
-		current = current->next;
+		current = (*current).next;
 	}
 	return (count);
 }
