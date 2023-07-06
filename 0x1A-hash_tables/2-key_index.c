c0e8f164a1bb00f42ab772ee546cdf16a364c108
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-07-06 11:26:45 -0700

    task2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - a function
+ * @key: param
+ * @size: param
+ * Return:0
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+}
