8959f50b2dc75311f76aadd5c82e6e60bcd63326
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-07-07 10:12:26 -0700

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,47 +1,50 @@
 #include "hash_tables.h"
-#include "stddef.h"
-#include "stdlib.h"
 
 /**
- * hash_table_set - a function
- * @key:: parameter
- * @value: parameter
- * @ht: param
- * Return: 0
+ * hash_table_set - a function that adds an element to the hash table
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key
+ * Value must be duplicated. value can be an empty string
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (ht == NULL || key == NULL || *key == '\0')
-	{
-		return (0);
-	}
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	node_t *new_node = malloc(sizeof(node_t));
+	unsigned long int index;
+	hash_node_t *new_node, *ptr;
 
-	if (new_node == NULL)
-	{
+	if (ht == NULL || strcmp(key, "") == 0 || key == NULL || value == NULL)
 		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	ptr = ht->array[index];
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+		{ free(ptr->value);
+			ptr->value = strdup(value);
+			if (ptr->value == NULL)
+				return (0);
+			return (1);
+		} ptr = ptr->next;
 	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	if (new_node->key == NULL || (value != NULL && new_node->value == NULL))
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
 
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
 	return (1);
 }
