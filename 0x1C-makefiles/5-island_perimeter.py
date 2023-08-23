41fef66d4ee0447d8369308eca31d4b6b009ba08
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-23 08:26:25 -0700

    tasks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,7 +11,7 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
 
-                if row > 0 and grid[row -1][col] == 1:
+                if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
