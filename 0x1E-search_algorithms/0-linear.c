a4a35cd7b7dfa65d3a09baa9b5dba280bc3ad425
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-10-09 16:29:37 +0100

    tasks

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+
+/**
+  * linear_search - a search funtion
+  * @array: parameter
+  * @size: parameter
+  * @value: parameter
+  * Return: 0
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	return (-1);
+}
