feae79dac0cc0f118d1c7d11e742d6850d947408
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-10-10 15:00:51 +0100

    tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,48 +4,35 @@
  * @array: parameter
  * @size: parameter
  * @value: parameter
+ * Return: 0
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid = 0;
-    size_t i;
-    
-    if (array == NULL || size == 0)
-    {
-        return(-1);
-    }
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid = 0;
+	size_t i;
 
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i < right)
-            {
-                printf("%d, ", array[i]);
-            }
-            else
-            {
-                printf("%d", array[i]);
-            }
-        }
-        printf("\n");
-        mid = left + (right - left) / 2;
-        if (array[mid] == value)
-        {
-            return(mid);
-        }
-        else if (array[mid] < value)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-
-    }
-    return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i < right)
+				printf("%d, ", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+		printf("\n");
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
